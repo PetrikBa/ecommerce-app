bbde199cfe3452c3a68a4d827f094d5109de1161
Peter Baca <petrik.baca@gmail.com>
2026-05-03 10:09:33 +0200

    progress

--- apps/client/src/app/products/page.tsx
+++ apps/client/src/app/products/page.tsx
@@ -1,11 +1,17 @@
 import ProductList from "@/components/ProductList";
 
-const ProductsPage = async ({searchParams}:{searchParams: Promise<{category:string}>}) => {
+const ProductsPage = async ({
+  searchParams
+}:{
+  searchParams: Promise<{category:string , search?: string, sort?: string}>
+}) => {
 
   const category = (await searchParams).category;
+  const search = (await searchParams).search;
+  const sort = (await searchParams).sort;
   return (
     <div className="">
-        <ProductList category={category} params="products" />
+        <ProductList category={category} search={search} sort={sort} params="products" />
     </div>
   )
 }
